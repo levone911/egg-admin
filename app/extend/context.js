5c42407e7c17944ce46fd784d7355216e56a9c3f
levone <downpu@163.com>
2017-11-24 18:21:48 +0800

    sidebar异步渲染

--- app/extend/context.js
+++ app/extend/context.js
@@ -1,19 +1,23 @@
 'use strict';
 module.exports = {
   async renderLayout(tpl, datas) {
-    try {
-      const data = await new Promise((resolve, reject) => {
-        try {
-          const data = this.renderView(tpl, datas);
-          resolve(data);
-        } catch (err) {
-          reject(err);
-        }
-      });
-      const layout = this.app.config.latoutPath;
-      await this.render(layout.admin, { body: data });
-    } catch (err) {
-      console.log(err);
-    }
+    const data = await new Promise((resolve, reject) => {
+      try {
+        const data = this.renderView(tpl, datas);
+        resolve(data);
+      } catch (err) {
+        reject(err);
+      }
+    });
+    const menu = await new Promise((resolve, reject) => {
+      try {
+        const data = this.renderView('/admin/common/sidebar.html', { test: '1111' });
+        resolve(data);
+      } catch (err) {
+        reject(err);
+      }
+    });
+    const layout = this.app.config.latoutPath;
+    await this.render(layout.admin, { body: data, sidebar: menu });
   },
 };
