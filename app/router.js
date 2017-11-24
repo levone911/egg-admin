c2547f3635d5a92cf7d46e4cc09324abef940712
levone <downpu@163.com>
2017-11-24 18:05:55 +0800

    dataTable实例

--- app/router.js
+++ app/router.js
@@ -1,6 +1,9 @@
 'use strict';
 
 module.exports = app => {
+  // h5部分
   app.get('/', app.controller.home.index);
+  // admin部分
   app.get('/admin', app.controller.admin.index);
+  app.get('/api/dataTable/test', app.controller.admin.dataTable);
 };
